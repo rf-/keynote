1170366d235556a216efeac75f0087b0c9546176
Perry Smith <pedz@easesoftware.com>
2013-02-02 14:01:48 -0600

    Added h2 h3 h4 and h5 and refilled the paragraph

--- lib/keynote/rumble.rb
+++ lib/keynote/rumble.rb
@@ -171,12 +171,13 @@ module Keynote
     COMPLETE = %w[a abbr acronym address applet area article aside audio b base
       basefont bdo big blockquote body br button canvas caption center cite
       code col colgroup command datalist dd del details dfn dir div dl dt em
-      embed fieldset figcaption figure font footer form frame frameset h1
-      h6 head header hgroup hr i iframe img input ins keygen kbd label legend
-      li link map mark menu meta meter nav noframes noscript object ol optgroup
-      option output p param pre progress q rp rt ruby s samp script section
-      select small source span strike strong style sub summary sup table tbody
-      td textarea tfoot th thead time title tr tt u ul var video wbr xmp]
+      embed fieldset figcaption figure font footer form frame frameset h1 h2 h3
+      h4 h5 h6 head header hgroup hr i iframe img input ins keygen kbd label
+      legend li link map mark menu meta meter nav noframes noscript object ol
+      optgroup option output p param pre progress q rp rt ruby s samp script
+      section select small source span strike strong style sub summary sup
+      table tbody td textarea tfoot th thead time title tr tt u ul var video
+      wbr xmp]
 
     # @private
     SELFCLOSING = %w[base meta link hr br param img area input col frame]
