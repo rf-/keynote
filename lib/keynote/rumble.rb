0b014cfdbccc690116327feb6ee806da162ade07
Ryan Fitzgerald <rwfitzge@gmail.com>
2013-02-02 21:01:48 -0800

    Remove overlapping tags from Keynote::COMPLETE

--- lib/keynote/rumble.rb
+++ lib/keynote/rumble.rb
@@ -168,15 +168,13 @@ module Keynote
 
     # A more complete set of HTML5 tags. You can use these by calling
     # `use_html_5_tags` in a presenter's class body.
-    COMPLETE = %w[a abbr acronym address applet area article aside audio b base
-      basefont bdo big blockquote body br button canvas caption center cite
-      code col colgroup command datalist dd del details dfn dir div dl dt em
-      embed fieldset figcaption figure font footer form frame frameset h1
-      h6 head header hgroup hr i iframe img input ins keygen kbd label legend
-      li link map mark menu meta meter nav noframes noscript object ol optgroup
-      option output p param pre progress q rp rt ruby s samp script section
-      select small source span strike strong style sub summary sup table tbody
-      td textarea tfoot th thead time title tr tt u ul var video wbr xmp]
+    COMPLETE = %w[abbr acronym address applet area article aside audio base
+      basefont bdo big blockquote body canvas caption center cite code col
+      colgroup command datalist dd details dfn dir dl dt embed fieldset
+      figcaption figure font footer frame frameset head header hgroup iframe
+      ins keygen kbd legend map mark menu meta meter nav noframes noscript
+      object output param progress q rp rt ruby s samp section small source
+      strike style summary title tt u var video wbr xmp]
 
     # @private
     SELFCLOSING = %w[base meta link hr br param img area input col frame]
