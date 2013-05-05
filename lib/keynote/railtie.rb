791af05d305e629fb73bafcab668cbba5a934109
Ryan Fitzgerald <rwfitzge@gmail.com>
2013-05-05 00:23:59 -0700

    Very minor refactoring

--- lib/keynote/railtie.rb
+++ lib/keynote/railtie.rb
@@ -6,23 +6,8 @@ module Keynote
   # @private
   class Railtie < Rails::Railtie
     config.after_initialize do |app|
-      if ::Rails.version.to_f >= 4
-        app.config.paths.add 'app/presenters'
-      else
-        app.config.paths.add 'app/presenters', :eager_load => true
-      end
-
-      if defined?(RSpec::Rails) && RSpec.respond_to?(:configure)
-        require 'keynote/testing/rspec'
-      end
-
-      if defined?(MiniTest::Rails)
-        require 'keynote/testing/minitest_rails'
-      end
-
-      if !defined?(MiniTest::Rails)
-        require "keynote/testing/test_unit"
-      end
+      add_presenters_to_paths(app)
+      load_test_integration
     end
 
     ActiveSupport.on_load(:action_view) do
@@ -40,6 +25,28 @@ module Keynote
     rake_tasks do
       if defined?(MiniTest::Rails)
         load File.expand_path("../testing/minitest_rails.rake", __FILE__)
+      end
+    end
+
+    def self.add_presenters_to_paths(app)
+      if ::Rails.version.to_f >= 4
+        app.config.paths.add 'app/presenters'
+      else
+        app.config.paths.add 'app/presenters', :eager_load => true
+      end
+    end
+
+    def self.load_test_integration
+      if defined?(RSpec::Rails) && RSpec.respond_to?(:configure)
+        require 'keynote/testing/rspec'
+      end
+
+      if defined?(MiniTest::Rails)
+        require 'keynote/testing/minitest_rails'
+      end
+
+      if !defined?(MiniTest::Rails)
+        require "keynote/testing/test_unit"
       end
     end
   end
