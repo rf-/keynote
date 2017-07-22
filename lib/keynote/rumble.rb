rf-/keynote

commit 05be4356b0a6ca7de48da926a9b997beb5ffeb4a

Always escape quotes in attributes; support array attrs
The current behavior is to pass HTML-safe strings through unaltered, but
this can still cause an XSS vulnerability if the string has an unescaped
quote in it:

    https://github.com/rails/rails/commit/4394e9075189ee6031944491aecd64fb269cdf54

I also added support for array attributes, since I noticed Rails does
it when I was copying their escaping code.

lib/keynote/rumble.rb
@@ -323,10 +323,18 @@ module Keynote
 
       def attrs_to_s
         attributes.inject("") do |res, (name, value)|
-          if value
-            value = (value == true) ? name : Rumble.html_escape(value)
-            res << " #{name}=\"#{value}\""
-          end
+          next unless value
+
+          value =
+            if value.is_a?(Array)
+              value.map { |val| Rumble.html_escape(val) }.join(" ")
+            elsif value == true
+              name
+            else
+              Rumble.html_escape(value)
+            end
+
+          res << " #{name}=\"#{value.gsub('"'.freeze, '&quot;'.freeze)}\""
           res
         end
       end

spec/rumble_spec.rb
@@ -67,11 +67,23 @@ class TestRumble < klass
 
   def test_hash_data
     str = <<-HTML
-      <div data-modal="true" data-test="&quot;test&quot;"></div>
+      <div data-modal="true" data-safe="&quot;&quot;&quot;" data-unsafe="&quot;&amp;quot;&quot;">
+      </div>
     HTML
 
     assert_rumble str do
-      div data: { modal: true, test: '"test"' }
+      div data: { modal: true, safe: '"&quot;"'.html_safe, unsafe: '"&quot;"' }
+    end
+  end
+
+  def test_array_attrs
+    str = <<-HTML
+      <div class="hello &quot;uns&amp;amp;fe&quot; &quot;w&amp;rld&quot;">
+      </div>
+    HTML
+
+    assert_rumble str do
+      div class: ["hello", '"uns&amp;fe"', '"w&amp;rld"'.html_safe]
     end
   end
 

commit 88c6cd9dc7544c92dc5da52614fecbb80166e0ab

Rumble: Fix nil attributes
This fixes a regression caused by 05be4356b0a6ca7de4. If a Rumble tag
has a `nil` value for a given attribute, it's supposed to omit the
attribute; the version of this code in 05be4356b0a6ca7de4 would raise
instead.

lib/keynote/rumble.rb
@@ -271,6 +271,7 @@ module Keynote
         if attrs && attrs[:data].is_a?(Hash)
           attrs = attrs.dup
           attrs.delete(:data).each do |key, value|
+            next if value.nil?
             attrs[:"data-#{key}"] = value.to_s
           end
         end
@@ -323,7 +324,7 @@ module Keynote
 
       def attrs_to_s
         attributes.inject("") do |res, (name, value)|
-          next unless value
+          next res unless value
 
           value =
             if value.is_a?(Array)

spec/rumble_spec.rb
@@ -72,7 +72,12 @@ class TestRumble < klass
     HTML
 
     assert_rumble str do
-      div data: { modal: true, safe: '"&quot;"'.html_safe, unsafe: '"&quot;"' }
+      div data: {
+        modal: true,
+        safe: '"&quot;"'.html_safe,
+        absent: nil,
+        unsafe: '"&quot;"'
+      }
     end
   end
 
@@ -84,6 +89,16 @@ class TestRumble < klass
 
     assert_rumble str do
       div class: ["hello", '"uns&amp;fe"', '"w&amp;rld"'.html_safe]
+    end
+  end
+
+  def test_nil_attr
+    str = <<-HTML
+      <div id="id" class="class"></div>
+    HTML
+
+    assert_rumble str do
+      div id: "id", title: nil, class: "class"
     end
   end
 